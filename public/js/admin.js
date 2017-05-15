458905a4bc464703814ac2fabe1956e2fb4855c6
HuangQingyi <297947067@qq.com>
2017-05-15 17:28:28 +0800

    bower install 的依赖和一些本地样式后用户逻辑 静态文件 图片等

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,7 +1,7 @@
 // 处理删除数据的逻辑
 $(function () {
-    //拿到所有删除按钮
-    $('.del').click(function (e) {
+    //拿到文章删除按钮
+    $('.delPassage').click(function (e) {
         var target = $(e.target);//this.target当前点击的按钮
         var id = target.data('id');
         var tr = $('.item-id-' + id);//拿到list页面中对应那行
@@ -9,6 +9,25 @@ $(function () {
         $.ajax({
             type: 'DELETE', // 异步请求类型：删除
             url: '/admin/passage/list?id=' + id,
+        })
+        //删除以后服务器返回一个状态台
+        .done(function (results) {
+            if (results.success === 1) {
+                if (tr.length > 0) {//如果这行有东西就remove掉
+                    tr.remove();
+                }
+            }
+        });
+    });
+    //拿到用户删除按钮
+    $('.delUser').click(function (e) {
+        var target = $(e.target);//this.target当前点击的按钮
+        var id = target.data('id');
+        var tr = $('.item-id-' + id);//拿到list页面中对应那行
+
+        $.ajax({
+            type: 'DELETE', // 异步请求类型：删除
+            url: '/admin/user/list?id=' + id,
         })
         //删除以后服务器返回一个状态台
         .done(function (results) {
--- public/js/detail.js
+++ public/js/detail.js
@@ -28,4 +28,15 @@ $(function() {
       }).appendTo('#commentForm')
     }
   })
+  //-点击回复改变样式
+  var replyDom = document.getElementsByClassName('replyComment');
+  var i;
+  for (i = 0;i < replyDom.length; i++){
+    replyDom[i].onclick = function(e){
+      var replyButton = document.getElementsByClassName('replyButton')[0];
+      replyButton.innerHTML='提交回复内容';
+    }
+  }
+
+
 })
