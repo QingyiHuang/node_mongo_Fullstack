605e593b1876d34ffafd0407a7564fba53d0dec9
HuangQingyi <297947067@qq.com>
2017-05-19 09:57:38 +0800

    gulp 对 js检查

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require('gulp-jshint')
 
 
 gulp.task('jshint', function () {
-    gulp.src('js/*.js')
+    gulp.src('./public/js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter()); // 输出检查结果
 });
